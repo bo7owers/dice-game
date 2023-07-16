567d8e2e28db620f756b14a27ffaa5d524606578
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-07-16 16:27:54 -0400

    test new cube

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import Die from '../components/Die.vue'
+import Dietwo from '@/components/Dietwo.vue'
 </script>
 
 <template>
-    <main><Die /></main>
+    <main>
+        <Die />
+        <Dietwo />
+    </main>
 </template>
